6e5b24c9e4a84ea70beddb6b712d4096878c7d0e
Matteo Matteob99 Bocci <matteo.bocci+git@gmail.com>
2021-05-16 23:31:34 +0200

    fix run.sh

--- tinc_client/run.sh
+++ tinc_client/run.sh
@@ -3,8 +3,8 @@
 TINC_NAME=$(bashio::config "tinc_name")
 TINC_FOLDER=$(bashio::config "config_folder")
 echo "$TINC_NAME $TINC_FOLDER"
-while true;
-do
-	sleep 3;
-done
-tincd -D -n  $TINC_NAME -c  $TINC_FOLDER
+#while true;
+#do
+#	sleep 3;
+#done
+tincd -D -n  $TINC_NAME -c  /config/$TINC_FOLDER
