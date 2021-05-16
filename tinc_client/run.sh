994b12b08fe4240370205c79dde1f34558ef7bc1
Matteo Matteob99 Bocci <matteo.bocci+git@gmail.com>
2021-05-16 23:25:12 +0200

    debug time

--- tinc_client/run.sh
+++ tinc_client/run.sh
@@ -2,4 +2,9 @@
 
 TINC_NAME=$(bashio::config "tinc_name")
 TINC_FOLDER=$(bashio::config "config_folder")
+echo "$TINC_NAME $TINC_FOLDER"
+while true;
+do
+	sleep 3;
+done
 tincd -D -n  $TINC_NAME -c  $TINC_FOLDER
